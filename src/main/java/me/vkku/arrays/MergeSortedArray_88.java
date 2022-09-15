ec27e4a17cf87358c9452a1d11da49e7a09a6717
Vivek Kumar <contactviku@gmail.com>
2022-09-16 01:08:11 +0530

    Cleaned Code

--- src/main/java/me/vkku/arrays/MergeSortedArray_88.java
+++ src/main/java/me/vkku/arrays/MergeSortedArray_88.java
@@ -16,17 +16,26 @@ public class MergeSortedArray_88 {
   @Test
   public void merge(int[] nums1, int m, int[] nums2, int n) {
     int length = m + n - 1, ptr1 = m - 1, ptr2 = n - 1;
-    while (length >= 0) {
-      if (ptr2 == -1) {
+    while(ptr1 >= 0 && ptr2 >= 0){
+      nums1[length--] = nums1[ptr1] > nums2[ptr2] ? nums1[ptr1--] : nums2[ptr2--];
+    }
+    while (ptr2 >= 0){
+      nums1[length--] = nums2[ptr2--];
+    }
+  }
+
+  /* Faster Code
+    int length = m + n - 1, ptr1 = m-1, ptr2 = n-1;
+    while (length >= 0){
+      if(ptr2 == -1)
         return;
-      }
-      if (ptr1 >= 0 && nums1[ptr1] > nums2[ptr2]) {
+      if(ptr1 >= 0 && nums1[ptr1] > nums2[ptr2]){
         nums1[length--] = nums1[ptr1--];
-      } else {
+      }else{
         nums1[length--] = nums2[ptr2--];
       }
     }
-  }
+   */
 
   @Test
   public void driver() {
