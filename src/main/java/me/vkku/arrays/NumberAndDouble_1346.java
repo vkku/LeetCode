d3e36ad3d6d84b34c762840984411e05694e0d7f
Vivek Kumar <contactviku@gmail.com>
2022-09-22 00:04:51 +0530

    Add meta

--- src/main/java/me/vkku/arrays/NumberAndDouble_1346.java
+++ src/main/java/me/vkku/arrays/NumberAndDouble_1346.java
@@ -8,6 +8,25 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import sun.security.util.ArrayUtil;
 
+/*
+https://leetcode.com/problems/check-if-n-and-its-double-exist/
+Given an array arr of integers, check if there exist two indices i and j such that :
+
+i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
+
+
+Example 1:
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+
+Example 2:
+Input: arr = [3,1,7,11]
+Output: false
+Explanation: There is no i and j that satisfy the conditions.
+ */
 public class NumberAndDouble_1346 {
 
   @Test
