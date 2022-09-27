cc2f66744e56407306a5f3a30dd1c1684ea0e7e5
Vivek Kumar <contactviku@gmail.com>
2022-09-27 21:35:33 +0530

    Update MoveZeroesToEnd_283.java

--- src/main/java/me/vkku/arrays/MoveZeroesToEnd_283.java
+++ src/main/java/me/vkku/arrays/MoveZeroesToEnd_283.java
@@ -1,7 +1,20 @@
 package me.vkku.arrays;
 
 import org.junit.jupiter.api.Test;
+/*
+https://leetcode.com/problems/move-zeroes/submissions/
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
+Note that you must do this in-place without making a copy of the array.
+
+Example:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ */
 public class MoveZeroesToEnd_283 {
 
   @Test
