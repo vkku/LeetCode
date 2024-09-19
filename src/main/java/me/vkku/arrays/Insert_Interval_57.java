ec09611bf3145c849a026cc578b069cec1475655
Vivek Kumar <contactviku@gmail.com>
2024-09-19 12:54:12 +0530

    Clean code + Explanation

--- src/main/java/me/vkku/arrays/Insert_Interval_57.java
+++ src/main/java/me/vkku/arrays/Insert_Interval_57.java
@@ -4,11 +4,36 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+    # Intuition
+    <!-- Describe your first thoughts on how to solve this problem. -->
+    Interval Problem pattern:
+    Since it's sorted, simple cases first and rest follows(in else)
+    1. Break problem into cases
+    2. Consolidate the cases till minimal set is reached
+    # Approach
+    <!-- Describe your approach to solving the problem. -->
+    Create a result list and iterate with intervals. Either add interval based on below scenarios or stretch interval.
+
+    There can be multiple scenarios
+    ![57.jpg](https://assets.leetcode.com/users/images/14212dae-01e7-4c5a-93c3-0f61eaa11186_1726730363.7825503.jpeg)
+
+    But, these scenarios contain all :
+    1. new interval happens before
+    2. new interval happens after
+    3. new interval happens anywhere else, in that case just keep stretching new interval and don't add it to result list inside loop
+    # Complexity
+    - Time complexity:
+    <!-- Add your time complexity here, e.g. $$O(n)$$ -->
+    O(n)
+    - Space complexity:
+    <!-- Add your space complexity here, e.g. $$O(n)$$ -->
+    O(n)
+ */
 
 public class Insert_Interval_57 {
     List<int[]> result = new ArrayList<>();
@@ -17,17 +42,29 @@ public class Insert_Interval_57 {
         List<int[]> result = new ArrayList<>();
         if(intervals.length > 0 && intervals[0].length > 0){
             for(int i = 0 ; i < intervals.length ; i++){
+                //Scenario 1 : new interval happens before current interval
                 if(newInterval[1] < intervals[i][0]){
                     result.add(newInterval);
+                    /*
                     for(int j = i ; j < intervals.length ; j++){
                         result.add(intervals[j]);
                     }
+                    */
+                    //Simplified version of above commented code
+                    result.addAll(List.of(Arrays.copyOfRange(intervals, i, intervals.length)));
                     return result.toArray(new int[0][]);
-                }else if(newInterval[0] > intervals[i][1]){
+                }
+
+                //Scenario 2 : new interval happens after current interval
+                else if(newInterval[0] > intervals[i][1]){
+                    //Adding current interval only since there can be intervals ahead which will be merged into new interval
                     result.add(intervals[i]);
-                }else{
-                    newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
-                    newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+                }
+
+                //Scenario 3 : new interval is anywhere eles viz. included in current interval(touching end/start or completely included), extend new interval in this scenario
+                else{
+                    newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                    newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
                 }
             }
         }
